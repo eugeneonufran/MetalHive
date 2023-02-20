b7e066a84037354fcb3c928234fff9eb802982a5
eugeneonufran <eugeneonufran@gmail.com>
2023-02-20 11:21:08 +0200

    Added API key

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using MetalHive.Data.DataModel;
 using MetalHive.Data.DataServices.Interfaces;
 using MetalHive.Data.DataServices;
 using Microsoft.EntityFrameworkCore;
+using MetalHive.Middleware;
+using MetalHive;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.OperationFilter<SwaggerParameters>();
+
+});
 
 
 var connectionString = builder.Configuration.GetConnectionString("ConStr");
@@ -19,17 +25,25 @@ builder.Services.AddTransient<IContractDataService, ContractDataService>();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
+
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseMiddleware<ApiKeyMiddleware>();
 
 app.Run();
---
+++ SwaggerParameters.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MetalHive
+{
+    public class SwaggerParameters : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "XApiKey",
+                In = ParameterLocation.Header,
+                Required = true
+            });
+        }
+    }
+}
